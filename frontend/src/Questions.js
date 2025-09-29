d4675d410c16e81bac79345de64575827b93ec23
Gabriella Rosal Calit <grcalit@users.noreply.github.com>
2025-09-29 07:26:43 -0700

    forgot to add the frontend file

--- frontend/src/Questions.js
+++ frontend/src/Questions.js
@@ -3,32 +3,39 @@ import {useState, useEffect} from 'react';
 
 export default function Question({topicId}) {
     const qList = topics[topicId-1].questions;
-    const [ans, setAns] = useState(" ");
-    const [ansID, setAnsId] = useState("");
+    const [ans, setAns] = useState(() => {
+        let data = {};
+        for (let i = 0; i < qList.length; i++) {
+        data[qList[i].id] = "";
+        }
+        return data;
+    });
 
+    function changeValue(id, value) {
+        setAns(prev => ({
+        ...prev,
+        [id]: value
+        }));
+    }
 
     useEffect(() => {
         if (!ans) return;
 
-        const formData = {
-            id: ansID,
-            answer: ans};
-
         fetch(`http://localhost:8080/api/topic${topicId}`, {
             method: "POST",
             headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(formData)
+            body: JSON.stringify(ans)
         })
         .then(res => res.text())
         .then(data => console.log("Saved: ", data))
         .catch(err => console.error("Error: ", err));
-    }, [ans, ansID])
+    }, [ans])
 
     return (
         qList.map(question => (
             <div key={question.id}>
                 <p>{question.id + '. ' + question.text}</p>
-                <select value={ans} onChange={e => {setAns(e.target.value); setAnsId(question.id);}}>
+                <select value={ans[question.id] || ""} onChange={(e) => changeValue(question.id, e.target.value)}>
                     <option value=" ">Select an option</option>
                     {question.options.map(opt => <option key={opt} value={opt}>{opt}</option>)}
                 </select>
@@ -36,4 +43,6 @@ export default function Question({topicId}) {
         ))
     )
 }
+
+
 
